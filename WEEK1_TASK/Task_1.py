b2f15aed242f23cecb2eba2876ac716c13830812
GogikarLahiri <gogikarlahiri@gmail.com>
2025-09-26 19:30:49 +0530

    The Developer Arena Internship Week one task

---
+++ WEEK1_TASK/Task_1.py
@@ -0,0 +1,17 @@
+# Program on int, float, string, boolean
+
+# Integer
+roll_number = 101
+print("Roll Number:", roll_number)
+
+# Float
+cgpa = 8.7
+print("CGPA:", cgpa)
+
+# String
+college = "Sri Indu College"
+print("College Name:", college)
+
+# Boolean
+has_passed = True
+print("Has Passed?", has_passed)
